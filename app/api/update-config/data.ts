4f8a22a36794d3d5306bc80fe4c6fcb0d76e1551
Bibek Joshi <bibekjoshi34@gmail.com>
2026-03-06 23:23:04 +0545

    update app version

--- app/api/update-config/data.ts
+++ app/api/update-config/data.ts
@@ -1,6 +1,6 @@
 export const appUpdateConfig = {
   latestVersion: "1.6.0",
-  minSupportedVersion: "1.6.0",
+  minSupportedVersion: "1.5.0",
   androidStoreUrl: "market://details?id=com.udharo.app",
   iosStoreUrl: "https://apps.apple.com/app/id123456789",
 };
